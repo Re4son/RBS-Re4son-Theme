bcb2a831045b298cad3af4992080db52cfdd891c
Re4son <Re4son@users.noreply.github.com>
2017-09-17 16:56:14 +1000

    Initial commit

---
+++ shell-theme/extractgst.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+workdir=`pwd`
+if [ ! -d ${workdir}/theme ]; then
+  mkdir -p ${workdir}/theme
+fi
+gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
+
+for r in `gresource list $gst`; do
+        gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+done
